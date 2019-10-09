b8f0bccd681579d05f8fa90a93689bfc828ee77b
おう <15621624+xbw@users.noreply.github.com>
2019-10-09 17:37:25 +0800

    org.json Demo

---
+++ json/src/main/java/com/xbw/json/org/json/Json.java
@@ -0,0 +1,38 @@
+package com.xbw.json.org.json;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.xbw.json.JsonBean;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Json {
+
+	public static void main(String[] args) {
+		List<JsonBean> list = new ArrayList<JsonBean>();
+		JsonBean bean = new JsonBean(0, "bean0", new Date(System.currentTimeMillis()));
+		json(bean);
+
+		list.add(bean);
+		for (int i = 1; i < 3; i++) {
+			list.add(new JsonBean(i, "bean" + i, new Date(System.currentTimeMillis())));
+		}
+		json(list);
+	}
+
+	public static void json(JsonBean bean) {
+		log.info("toJson - {}", JSONObject.valueToString(bean));
+		log.info("toJson - {}", new JSONObject(bean));
+	}
+
+	public static void json(List<JsonBean> list) {
+		log.info("toJson - {}", JSONObject.valueToString(list));
+		log.info("toJson - {}", new JSONArray(list));
+	}
+}
